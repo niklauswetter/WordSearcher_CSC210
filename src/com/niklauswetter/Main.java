1cf553d11d1e1779b1355c1e7670807c9a9f2f71
nickwetter <nickwetter99@gmail.com>
2021-08-30 16:26:39 -0700

    First mac commit

--- src/com/niklauswetter/Main.java
+++ src/com/niklauswetter/Main.java
@@ -3,6 +3,10 @@ package com.niklauswetter;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String dictPath, puzzlePath;
+        int rows, cols;
+        //Create scanner object
+        //Create file object
+        //Create char array
     }
 }
