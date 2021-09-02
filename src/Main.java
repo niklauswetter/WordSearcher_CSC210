3aa50b380d186441e51b37e783986d651199f77a
Nick <nickwetter99@gmail.com>
2021-09-01 20:05:36 -0700

    9/1/2021-- 8:04 -Added boilerplate file and scanner code - Added files into src directory * Added files into proper directory lol

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+        //Initialize variables
+        String dictPath, puzzlePath;
+        int rows, cols;
+        //Store CL args
+        dictPath = args[0];
+        puzzlePath = args[1];
+        //DEBUG DELETE
+        System.out.println(dictPath+" "+puzzlePath);
+        //File Objects
+        File dict = new File(dictPath);
+        File puzzle = new File(puzzlePath);
+        //Scanner Objects
+        Scanner dictScanner = new Scanner(dict);
+        Scanner puzzleScanner = new Scanner(puzzle);
+        //Get grid dimensions
+        rows = puzzleScanner.nextInt();
+        cols = puzzleScanner.nextInt();
+        //DEBUG DELETE
+        System.out.println(rows+" "+cols);
+        //Create dictionary list
+        //Create char array
+    }
+}
