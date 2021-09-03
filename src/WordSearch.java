a4a3e3176fde68766a2fe06f25327b0c7f77e2b2
Nick <nickwetter99@gmail.com>
2021-09-02 23:35:11 -0700

    9/2/21--11:34PM -Renamed file for autograder

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class WordSearch {
 
     public static void main(String[] args) throws IOException
     {
