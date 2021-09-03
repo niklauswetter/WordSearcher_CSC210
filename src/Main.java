686f7d3b6908e7ef14700ee152b2b17a3b5399b2
Nick <nickwetter99@gmail.com>
2021-09-02 22:56:12 -0700

    9/2/21--10:55PM - Finds words in the four cardinal directions - Prints words after finding them

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class  Main {
+public class Main {
 
     public static void main(String[] args) throws IOException
     {
@@ -45,14 +45,121 @@ public class  Main {
                 puzzleChars[i][j]=temp[j];
         }
         puzzleScanner.close();
-        //DEBUG DELETE
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;j++)
-                System.out.print(puzzleChars[i][j]);
-            System.out.println();
-        }
+        //Create found words list
+        List<String> foundWords = new ArrayList<String>();
         //Begin searching for words
+        int counter =0;
+        searchLeftToRight(puzzleChars,dictionary,foundWords,rows,cols);
+        System.out.println("Left to right!");
+        for(int i=counter;i<foundWords.size();i++)
+        {
+            System.out.println(foundWords.get(i));
+            counter++;
+        }
+        searchRightToLeft(puzzleChars,dictionary,foundWords,rows,cols);
+        System.out.println("Right to left!");
+        for(int i = counter;i<foundWords.size();i++)
+        {
+            System.out.println(foundWords.get(i));
+            counter++;
+        }
+        searchTopToBottom(puzzleChars,dictionary,foundWords,rows,cols);
+        System.out.println("Top to bottom!");
+        for(int i = counter;i<foundWords.size();i++)
+        {
+            System.out.println(foundWords.get(i));
+            counter++;
+        }
+        searchBottomToTop(puzzleChars,dictionary,foundWords,rows,cols);
+        System.out.println("Bottom to top!");
+        for(int i = counter;i<foundWords.size();i++)
+        {
+            System.out.println(foundWords.get(i));
+            counter++;
+        }
     }
     //Declare search methods here
+    public static void searchLeftToRight(char[][] chars,List<String> dictionary,List<String> foundWords, int rows,int cols)
+    {
+        String hold = "";
+        for(int i = 0;i < rows; i++)
+        {
+            for(int j = 0; j< cols; j++)
+            {
+                for(int k=0+j;k<cols;k++)
+                {
+                    hold += chars[i][k];
+                    if(hold.length()<3)
+                        continue;
+                    if(dictionary.contains(hold))
+                        foundWords.add(foundWords.size(),hold);
+                }
+                hold="";
+            }
+            hold="";
+        }
+    }
+
+    public static void searchRightToLeft(char[][] chars,List<String> dictionary,List<String> foundWords, int rows,int cols)
+    {
+        String hold="";
+        for(int i = 0;i<rows;i++)
+        {
+            for(int j = cols;j>-1;j--)
+            {
+                for(int k=j-1;k>-1;k--)
+                {
+                    hold += chars[i][k];
+                    if(hold.length()<3)
+                        continue;
+                    if(dictionary.contains(hold))
+                        foundWords.add(foundWords.size(),hold);
+                }
+                hold="";
+            }
+            hold="";
+        }
+    }
+
+    public static void searchTopToBottom(char[][] chars,List<String> dictionary,List<String> foundWords, int rows,int cols)
+    {
+        String hold = "";
+        for(int i=0;i<cols;i++)
+        {
+            for(int j = 0; j< rows;j++)
+            {
+                for(int k = 0+j;k<rows;k++)
+                {
+                    hold += chars[k][i];
+                    if(hold.length()<3)
+                        continue;
+                    if(dictionary.contains(hold))
+                        foundWords.add(foundWords.size(),hold);
+                }
+                hold="";
+            }
+            hold="";
+        }
+    }
+
+    public static void searchBottomToTop(char[][] chars,List<String> dictionary,List<String> foundWords, int rows,int cols)
+    {
+        String hold = "";
+        for(int i = 0;i<cols;i++)
+        {
+            for(int j = rows; j>-1;j--)
+            {
+                for(int k = j-1;k>-1;k--)
+                {
+                    hold += chars[k][i];
+                    if(hold.length()<3)
+                        continue;
+                    if(dictionary.contains(hold))
+                        foundWords.add(foundWords.size(),hold);
+                }
+                hold="";
+            }
+            hold="";
+        }
+    }
 }
